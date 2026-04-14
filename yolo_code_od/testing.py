beca591d443b5e742900ca5389adf512a222476e
fenofista <s960068sss@gmail.com>
2026-04-14 10:59:13 +0800

    update

--- yolo_code_od/testing.py
+++ yolo_code_od/testing.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
 import torch
 
-device = "cuda"
+device = "mps" 
 
-name = "2026_03_11(1)"  # 模型路徑
+name = "2026_04_02(2)"  # 模型路徑
 
 model_path = f'yolo_output/runs/detect/{name}/weights/best.pt'
 # 驗證集資料夾（需有標註檔案如YOLO格式的labels）
-data_yaml = 'yolo_data/all_data_nodule_normal_cut/data.yaml'
+data_yaml = 'yolo_data/combined_with_synthesis_od_v5/data.yaml'
 
 model = YOLO(model_path)
 model.to(device)
@@ -15,5 +15,5 @@ model.to(device)
 
 # 評估模型
 classes = [0]  # 只評估結節類別（0）
-metrics = model.val(data=data_yaml, conf=0.5, iou=0.5, name = f"{name}_val_results", classes=classes)
+metrics = model.val(data=data_yaml, conf=0.2, iou=0.5, name = f"{name}_val_results", classes=classes)
 print(metrics.results_dict)
